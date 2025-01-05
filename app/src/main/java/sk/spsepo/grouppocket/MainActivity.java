d36b63f97d423aafdb7c27ff7f3cdcfa5f18c668
Martin-Benco <martinbencojr@gmail.com>
2025-01-05 21:28:32 +0100

    Po vytvoreni group page

--- app/src/main/java/sk/spsepo/grouppocket/MainActivity.java
+++ app/src/main/java/sk/spsepo/grouppocket/MainActivity.java
@@ -1,12 +1,18 @@
 package sk.spsepo.grouppocket;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Make status bar #141414 with white icons
+        getWindow().setStatusBarColor(Color.parseColor("#141414"));
+
+        // Make navigation bar black with white icons
+        getWindow().setNavigationBarColor(getResources().getColor(android.R.color.black));
+
+        // Configure system bars appearance
+        WindowInsetsControllerCompat windowInsetsController =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setAppearanceLightNavigationBars(false);  // Navigation bar icons white
+        windowInsetsController.setAppearanceLightStatusBars(false);   // Status bar icons white for visibility on dark background
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
